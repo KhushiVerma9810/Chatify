0bf0e2423cab9acde9e5323b2b82ead9282cff5d
Khushi Verma <khushiverma54583@gmail.com>
2023-03-08 21:37:00 +0530

    adding validation in register

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -1,33 +1,84 @@
-import React from 'react'
+import React , {useState , useEffect}from 'react'
 import { Link } from 'react-router-dom';
 import styled from "styled-components";
 import logo from "../assets/chat.png"
 const Register = () => {
+
+const [values , setValues]= useState({
+  username:"",
+  email:"",
+  password:"",
+  confirmpPassword:"",
+})
+
 const handleSubmit = (event)=>{
   event.preventDefault();
   alert("form");
 
 }
-const handleChange=(event)=>{
+const handlevalidation = () => {
+  let pass = document.getElementsByClassName('password');
+  pass.addEventListener("input", function(event) {
+    if (pass.validity.tooShort) {
+      pass.setCustomValidity("Password must be 6 or more characters");
+    } else {
+      pass.setCustomValidity("");
+    }
+  })
+  // if (pass.length < 6) {
+  //   // Add an event listener for the 'invalid' event
+  //   document.getElementById('password').addEventListener('invalid', function(event) {
+  //     // Set the custom validity message for the input
+  //     event.target.setCustomValidity('Password must contain 6 or more characters');
+  //   });
+  //   // Set the validity state of the input to false to trigger the 'invalid' event
+  //   document.getElementById('password').validity.valid = false;
+  // }
 
+  let usernameInput = document.getElementById('Username');
+      usernameInput.addEventListener('input', () => {
+        const usernameRegex = /^[A-Za-z]+$/;
+        if (!usernameRegex.test(usernameInput.value)) {
+          usernameInput.setCustomValidity('Username should only contain letters.');
+        } else {
+          usernameInput.setCustomValidity('');
+        }
+      })
+  } ;
+  // uname.oninvalid=function(event){
+  //   event.target.setCustomValidity('Username should only contain lowercase letters. e.g. khushi')
+  // }
+const handleChange=(event)=>{
+  setValues({...values,[event.target.name]:event.target.value});
 }
   return (
    <>
    <FormContainer>
-    <form onSubmit={(event)=>{handleSubmit(event)}}>
+    <form onSubmit={(event)=>{handleSubmit(event)}} onClick={(e)=>{handlevalidation(e)}}>
       <div className="brand">
         <img src={logo} alt="" />
         <h1>Chatify</h1>
       </div>
       <input type="text" placeholder='Username'
       name='username'
+      id='Username'
+      pattern="[A-Za-z]+"
+      required
+      onChange={(e)=>handleChange(e)}
+      />
+      <input type="email" placeholder='Email'
+      name='email'
       onChange={(e)=>handleChange(e)}
       />
       <input type="password" placeholder='Password'
       name='password'
+     className ='password'
+     minlength="6"
       onChange={(e)=>handleChange(e)}/>
 
       <input type="password" placeholder='Confirm Password'
+       minlength="6"
+       className='password'
       name='confirmpassword'
       onChange={(e)=>handleChange(e)}/>
      <button type='submit'>Create User</button>
